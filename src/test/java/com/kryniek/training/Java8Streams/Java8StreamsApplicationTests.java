a57889b7d52e7692ded777d0c1ae6e2999644a27
kszatk <Krystian.Szatkiewicz@acxiom.com>
2020-06-09 09:06:15 +0200

    initial

---
+++ src/test/java/com/kryniek/training/Java8Streams/Java8StreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kryniek.training.Java8Streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8StreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
